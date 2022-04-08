df3afc634fbd837ade8e40358ce4b93761e7c9a6
Erdianus <72244475+Erdianus@users.noreply.github.com>
2022-04-09 07:52:35 +0800

    percobaan

--- lib/main.dart
+++ lib/main.dart
@@ -7,118 +7,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Wisata Bandung',
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
-        home: FirstScreen());
+        home: DetailScreen());
   }
 }
 
-class Heading extends StatelessWidget {
-  final String text;
-
-  Heading({required this.text});
-
+class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-}
-
-class BiggerText extends StatefulWidget {
-  final String text;
-  const BiggerText({required this.text});
-  @override
-  _BiggerTextState createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(widget.text, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-          child: Text("Perbesar"),
-          onPressed: () {
-            setState(() {
-              _textSize = 32.0;
-            });
-          },
-        )
-      ],
-    );
-  }
-}
-
-class FirstScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var mediaQueryWidth = MediaQuery.of(context).size.width;
-    var mediaQueryHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-          onPressed: () {},
-        ),
-        title: Text('Posttest 1 Erdianus Pagesong'),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.search,
-                color: Colors.white,
-              ))
-        ],
-      ),
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.green, Color.fromARGB(255, 121, 158, 119)],
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black,
-                offset: Offset(3, 6),
-                blurRadius: 10,
-              )
-            ],
-          ),
-          child: Center(
-            child: Text(
-              'Hello Nama Aku Erdianus',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          width: mediaQueryWidth,
-          height: mediaQueryHeight,
-          padding: EdgeInsets.all(20),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    );
+        body: Column(
+      children: [Text('Farming House Lembang'), Text('Tes'), Text('Percobaan')],
+    ));
   }
-}
+} 
+//ini percobaan
