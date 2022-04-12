51a936e4433701a2b7d8da21c162b9ad182f8e6f
Erdianus <72244475+Erdianus@users.noreply.github.com>
2022-04-12 13:22:33 +0800

    menambahkan icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,85 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
-        home: Gambar());
+        home: DetailScreen());
   }
 }
+
+// class DropDown  extends StatefulWidget {
+//   @override
+//   _DropDownState createState() => _DropDownState();
+// }
+
+// class _State extends State<> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+
+//     );
+//   }
+// }
 
 class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [Text('Farming House Lembang'), Text('Tes'), Text('Percobaan')],
-    ));
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 16),
+              child: Text(
+                'Farming House Lembang',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.calendar_today),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Text("Open Everyday"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.access_time),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Text("09:00 - 20:00"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.monetization_on),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Text("RP 25.000"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -36,7 +104,7 @@ class Gambar extends StatelessWidget {
             Container(
               width: 200,
               height: 250,
-              margin: EdgeInsets.only(bottom: 14),
+              margin: EdgeInsets.all(14),
               color: Colors.black,
             ),
             Container(
